620bb4ec240584c5346650c28cda4d0518bd9b25
Jagannath Shingne <133676629+JagannathShingne01@users.noreply.github.com>
2023-11-18 22:36:00 +0530

    Update usePokemonList.js

--- pokedex/src/hooks/usePokemonList.js
+++ pokedex/src/hooks/usePokemonList.js
@@ -37,7 +37,6 @@ function usePokemonList() {
                 image:(pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default :  pokemon.sprites.front_shiny, 
                 types: pokemon.types
             }
-
         });
         console.log(pokeListRes)
         setPokemonListState((state)=>({
