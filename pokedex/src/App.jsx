128b20aa17cfbc07854c3891da9019fffcf6b117
JagannathShingne01 <jagannathshingne01@gmail.com>
2023-12-09 22:03:55 +0530

    css added

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -2,13 +2,14 @@
 import { Link } from 'react-router-dom';
 import './App.css'
 import CustomRoutes from './routes/CustomRoutes';
-
+import poke from "../public/HomePoke.png"
 function App() {
 
   return (
     <div className='outer-pokedex'>
     <h1 id="pokedex-heading">
       <Link to="/">Pokedex</Link>  
+      <img class="navimg" src="public/HomePoke.png" alt="Pokemon" />
     </h1>
       <CustomRoutes />
     </div>
